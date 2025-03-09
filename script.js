9af4f991d0df02d7da4c06501dc01e16bae0ec48
evelynluw510 <pikakumaa@gmail.com>
2025-03-09 08:01:49 +0000

    end

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+new fullpage('#fullpage', {
+    // Enables smooth scrolling between sections
+    autoScrolling: true,  
+    // Shows navigation dots at the top of the page
+    navigation: true,     
+  });
